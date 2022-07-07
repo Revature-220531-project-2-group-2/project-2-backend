cfca38df784477d195f9de3918de688344e1eae1
Josh Josh <jmt0494@gmail.com>
2022-07-07 14:13:39 -0500

    adding the blank project

---
+++ src/main/java/com/revature/DungensAndDragonsAppApplication.java
@@ -0,0 +1,13 @@
+package com.revature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DungensAndDragonsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DungensAndDragonsAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/revature/DungensAndDragonsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DungensAndDragonsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
