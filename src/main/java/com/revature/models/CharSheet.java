b54faf3f6fde0ed17104b565ac5c1570c507e5a5
Kenneth Burke <khtenne@gmail.com>
2022-07-15 21:46:11 -0700

    changed charsheet pk to reference the char name

--- src/main/java/com/revature/models/CharSheet.java
+++ src/main/java/com/revature/models/CharSheet.java
@@ -23,22 +23,26 @@ import lombok.NoArgsConstructor;
 @Entity
 @Data @NoArgsConstructor @AllArgsConstructor
 public class CharSheet{
+	//TODO start here and work with join table
+    @Id
+	@Column(name="char_name")
+	private String charName;
 	
 
-
-	@Id 
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	@Column(name="char_id")
-	private int charId;
-	
+//	@Id 
+//	@GeneratedValue(strategy=GenerationType.IDENTITY)
+//	@Column(name="char_id")
+//	private int charId;
+//	
 	
 	
 	@ManyToOne(cascade = CascadeType.ALL)
 	private User user;
 	
-	@Column(name="char_name")
-	private String charName;
 	
+//	@Column(unique = true, name="char_name")
+//	private String charName;
+//	
 	private String race;
 	
 	@Column(name = "class")
@@ -46,7 +50,8 @@ public class CharSheet{
 	
 	
 	@OneToOne(cascade = CascadeType.ALL)
-	@JoinTable(name="char_attrib", joinColumns = {@JoinColumn(name ="char_sheet_id", referencedColumnName = "char_id")},
+	@JoinTable(name="char_attrib", 
+		joinColumns = {@JoinColumn(name ="char_name", referencedColumnName = "char_name")},
 	inverseJoinColumns = {@JoinColumn(name ="attrib_id", referencedColumnName="attribute_id" )})
 	private Attribute attributes;
 	
